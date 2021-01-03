4627c0628dda906e14717e3ac4b9416e6f2926d9
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2021-01-03 14:05:19 +0200

    /lessons,/,/hello.md working

--- groupwork2/app.js
+++ groupwork2/app.js
@@ -17,17 +17,10 @@ app.get('/', function (request, response) {
 var publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
-// // Routes GET requests to /lessons to the request handler
-// app.get('/lessons', (req, res) => {
-//     res.json({
-//         Lessons: [
-//         {" t o p i c ’ ": "‘ math ’", "‘ l o c at i o n ’": "‘ London ’", "‘ p r i c e ’" : 100 } ,
-//         {"‘ t o p i c ’": "‘ math ’" , "‘ l o c at i o n ’" : "‘ L i v e r p o o l ’", "‘ p r i c e ’" : 80 } ,
-//         { "‘ t o p i c ’" : "‘ math ’" , "‘ l o c at i o n ’" : "‘ Oxford ’" , "‘ p r i c e ’" : 90 } ,
-//         { "‘ t o p i c ’" : "‘ math ’" , "‘ l o c at i o n ’" : "‘ B r i s t o l ’" , "‘ p r i c e ’" : 120 } ,
-// ]
-//     });
-// });
+// Routes GET requests to /lessons to the request handler
+app.get('/lessons', (req, res) => {
+    res.json(lessons);
+});
 
 app.use(function(req,res) {
     res.status(404).send('page not found)');
@@ -35,3 +28,9 @@ app.use(function(req,res) {
 http.createServer(app).listen(3000);
 console.log("app listens on port 3000");
 
+let lessons = [
+    { 't o p i c' :  'math ', ' l o c at i o n ': ' London ', ' p r i c e ': 100 },
+    { ' t o p i c ': ' math ', ' l o c at i o n ': ' L i v e r p o o l ', ' p r i c e ': 80 },
+    { ' t o p i c ': ' math ', ' l o c at i o n ': ' Oxford ', ' p r i c e ': 90 },
+    { ' t o p i c ': ' math ', ' l o c at i o n ': ' B r i s t o l ', ' p r i c e ': 120 },
+]
