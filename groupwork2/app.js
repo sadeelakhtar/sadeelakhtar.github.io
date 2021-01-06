d27cb0b7b21aee18ab348c06d3139d4013956e86
Syed Adeel Akhtar <syed.adeel17@imperial.ac.uk>
2021-01-06 15:17:09 +0000

    Refactored server and routing

--- groupwork2/app.js
+++ groupwork2/app.js
@@ -5,37 +5,35 @@ const path = require('path');
 const cors = require('cors');
 const mustache = require ('mustache');
 
-app.get(function (request, response, next) {
-    console.log("In comes a " + request.method + " to " + request.url);
-    next();
-});
-
-app.get('/', function (request, response) {
-    response.send('Hello,world!');
-});
-
 var publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
+
+app.get('/', function (request, response) {
+    response.send('Hello World!');
+});
 
 // Routes GET requests to /lessons and /users to the request handler
 app.get('/lessons', (req, res) => {
     res.json(lessons);
 });
+
 app.get('/users', (req, res) => {
     res.json(users);
 });
 
 app.use(function(req,res) {
-    res.status(404).send('page not found)');
+    res.status(404).send('Page not found');
 })
+
 http.createServer(app).listen(3000);
+
 console.log("app listens on port 3000");
 
 let lessons = [
-    { 't o p i c' :  'math ', ' l o c at i o n ': ' London ', ' p r i c e ': 100 },
-    { ' t o p i c ': ' math ', ' l o c at i o n ': ' L i v e r p o o l ', ' p r i c e ': 80 },
-    { ' t o p i c ': ' math ', ' l o c at i o n ': ' Oxford ', ' p r i c e ': 90 },
-    { ' t o p i c ': ' math ', ' l o c at i o n ': ' B r i s t o l ', ' p r i c e ': 120 },
+    {'topic':  'math', 'location': 'London', 'price': 100 },
+    {'topic': 'math', 'location': 'Liverpool ', 'price': 80 },
+    {'topic': 'math', 'location': 'Oxford', 'price': 90 },
+    {'topic': 'math', 'location': 'Bristol', 'price': 120 },
 ]
 
 let users = { 'email': 'user@email.com', 'password': 'mypassword' };
