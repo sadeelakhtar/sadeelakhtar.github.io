f52e2b60e6dbc71e26021e1a429f6ec863f603b9
Ignas Lamanauskas <il178@live.mdx.ac.uk>
2021-02-16 17:14:35 +0200

    added products

---
+++ groupwork3.1/products.js
@@ -0,0 +1,38 @@
+let products = [
+  {
+    "id": 1002,
+    "title": "Basketball",
+    "description": "Basketball lessons who want to be a <strong>NBA</strong> player!",
+    "price": 30.00,
+    "image": "images/Basketball.png",
+    "availableInventory": 7,
+    "rating": 5
+  },
+  {
+    "id": 1003,
+    "title": "Tennis",
+    "description": "Tennis lessons with proffessional coaches",
+    "price": 50.00,
+    "image": "images/Tennis.png",
+    "availableInventory": 5,
+    "rating": 4
+  },
+  {
+    "id": 1004,
+    "title": "Boxing",
+    "description": "A place where boys becomes mans",
+    "price": 25.00,
+    "image": "images/Boxing.png",
+    "availableInventory": 20,
+    "rating": 4
+  },
+  {
+    "id": 1005,
+    "title": "VolleyBall",
+    "description": "Be the best beach player!",
+    "price": 49.99,
+    "image": "images/Volleyball.png",
+    "availableInventory": 25,
+    "rating": 4
+  }
+]
